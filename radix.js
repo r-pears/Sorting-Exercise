9e3013283f25c5694ecbbe6cd04554f3fd1f5d02
Ryan <ryjen.pears@gmail.com>
2022-09-12 16:10:43 +0200

    radix sort added

--- radix.js
+++ radix.js
@@ -1,3 +1,38 @@
-function radixSort() {}
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+function digitCount(num) {
+  if (num === 0) return 1;
 
-module.exports = radixSort;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+
+  for (let i = 0; i < nums.length; i++){
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+
+  return maxDigits;
+}
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+
+  for (let i = 0; i < maxDigitCount; i++){
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+
+    for (let j = 0; j < nums.length; j++){
+      let num = nums[j];
+      let digit = getDigit(num, i);
+      digitBuckets[digit].push(num);
+    }
+
+    nums = [].concat(...digitBuckets);
+  }
+
+  return nums;
+ }
+
+module.exports = { getDigit, digitCount, mostDigits, radixSort };
